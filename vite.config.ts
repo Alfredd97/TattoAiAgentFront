8f586cdf14eab1b046b4b4e6ba94bf7a52a893a4
alfreddaguilar97 <alfreddaguilar97@gmail.com>
2025-04-19 20:09:54 +0000

    update build

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    host: true
-  }
-}) 
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === 'production';
+  return {
+    plugins: [react()],
+    base: isProduction ? '/minimalist-web/' : '/',
+    define: {
+      'process.env.NODE_ENV': isProduction ? '"production"' : '"development"',
+    },
+    server: {
+      port: 5173,
+      host: true,
+    },
+  };
+});
